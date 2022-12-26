c70b405a40521925853ded7c2818c1f301a99920
yly <fppuz123456@163.com>
2022-12-26 12:37:40 +0800

    [init]main类注解添加

--- src/main/java/com/yly/wiki/WikiApplication.java
+++ src/main/java/com/yly/wiki/WikiApplication.java
@@ -2,8 +2,14 @@ package com.yly.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+/*
+  这个注解其实加了一个 ComponentScan的注解,默认扫描当前包以及子包的类 , 因此一般我们要加上一个注解,默认扫描项目包
+  多包扫描支持{"1","2"}语法
+ */
+@ComponentScan("com.yly")
 public class WikiApplication {
 
     public static void main(String[] args) {
