71d0110e34ab9cd2f578a6bee579158cc1fa67ff
yly <fppuz123456@163.com>
2022-12-27 17:30:15 +0800

    [init]添加自定义组件,更换antdesign版本

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    YBook ©2022 Created by yaoliyan
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter',
+  props: {
+    msg: String,
+  },
+});
+</script>
+<style scoped>
+
+</style>
