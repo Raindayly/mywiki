1a33f82045b510c757d19040e88244aa116c373a
yly <fppuz123456@163.com>
2022-12-28 10:46:08 +0800

    [前端配置]axios拦截器,antdesign icon

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,9 +7,34 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import axios from 'axios';
+import * as Icons from '@ant-design/icons-vue';
 
 console.log(process.env.NODE_ENV)
 console.log(process.env.VUE_APP_SERVER)
 axios.defaults.baseURL = process.env.VUE_APP_SERVER
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
+
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+// 全局使用图标
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
