a20ab1da94b720903ae78b13bc950ffd69fa6f5e
yly <fppuz123456@163.com>
2022-12-28 10:24:53 +0800

    [init]动态sql编写

--- src/main/java/com/yly/wiki/service/EbookService.java
+++ src/main/java/com/yly/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.yly.wiki.req.EbookReq;
 import com.yly.wiki.resp.EbookResp;
 import com.yly.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -23,7 +24,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq ebookReq) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + ebookReq.getName() + "%");
+        if(!ObjectUtils.isEmpty(ebookReq.getName())) {
+            criteria.andNameLike("%" + ebookReq.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
         return respList;
