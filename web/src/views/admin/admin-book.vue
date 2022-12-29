8fb055c00ddd89c6903bc442d9bf3ae115feba86
yly <fppuz123456@163.com>
2022-12-29 09:53:15 +0800

    [前端业务]添加工具类, 添加电子书管理编辑

--- web/src/views/admin/admin-book.vue
+++ web/src/views/admin/admin-book.vue
@@ -40,18 +40,46 @@
         </a-tag>
       </span>
     </template>
-    <template #action>
+    <template #action="{ text, record }">
       <a-space>
-        <a-button type="primary">编辑</a-button>
+        <a-button type="primary" @click="edit(record)">编辑</a-button>
         <a-button type="primary" danger>删除</a-button>
       </a-space>
     </template>
   </a-table>
+
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover" />
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
+      </a-form-item>
+      <a-form-item label="分类">
+        <a-cascader
+            v-model:value="categoryIds"
+            :field-names="{ label: 'name', value: 'id', children: 'children' }"
+            :options="level1"
+        />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.description" type="textarea" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 
 <script lang="ts">
 import {defineComponent, onMounted, reactive, ref} from 'vue';
 import axios from "axios";
+import { message } from 'ant-design-vue';
+import {Tool} from "@/util/tool";
 
 
 const columns = [
@@ -104,6 +132,73 @@ export default defineComponent({
       total: 0
     })
 
+    // -------- 表单 ---------
+    /**
+     * 数组，[100, 101]对应：前端开发 / Vue
+     */
+    const categoryIds = ref();
+    const ebook = ref();
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
+      axios.post("/ebook/save", ebook.value).then((response) => {
+        modalLoading.value = false;
+        const data = response.data; // data = commonResp
+        if (data.success) {
+          modalVisible.value = false;
+
+          // 重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    /**
+     * 编辑
+     */
+    const edit = (record: any) => {
+      modalVisible.value = true;
+      ebook.value = Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
+    };
+
+    const level1 =  ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+
+          // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+          handleQuery({
+            page: 1,
+            size: pagination.value.pageSize,
+          });
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     const handleQuery = (params: any) => {
       loading.value = true
       axios.get('/ebook/list', {
@@ -125,17 +220,23 @@ export default defineComponent({
       })
     }
     onMounted(() => {
-      handleQuery({
-        page: 1,
-        size: pagination.value.pageSize
-      })
+      handleQueryCategory()
     })
     return {
       loading,
       listData,
       columns,
       pagination,
-      handleTableChange
+      handleTableChange,
+
+      edit,
+
+      ebook,
+      modalVisible,
+      modalLoading,
+      handleModalOk,
+      categoryIds,
+      level1,
     };
   },
   components: {
