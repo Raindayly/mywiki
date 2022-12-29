f9a0423cae6f93b297547d2488b758d6d0bcde9a
yly <fppuz123456@163.com>
2022-12-29 10:04:37 +0800

    [前端业务]列表展示代码回滚

--- web/src/views/admin/admin-book.vue
+++ web/src/views/admin/admin-book.vue
@@ -170,34 +170,34 @@ export default defineComponent({
       categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
     };
 
-    const level1 =  ref();
-    let categorys: any;
-    /**
-     * 查询所有分类
-     **/
-    const handleQueryCategory = () => {
-      loading.value = true;
-      axios.get("/category/all").then((response) => {
-        loading.value = false;
-        const data = response.data;
-        if (data.success) {
-          categorys = data.content;
-          console.log("原始数组：", categorys);
-
-          level1.value = [];
-          level1.value = Tool.array2Tree(categorys, 0);
-          console.log("树形结构：", level1.value);
-
-          // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
-          handleQuery({
-            page: 1,
-            size: pagination.value.pageSize,
-          });
-        } else {
-          message.error(data.message);
-        }
-      });
-    };
+    // const level1 =  ref();
+    // let categorys: any;
+    // /**
+    //  * 查询所有分类
+    //  **/
+    // const handleQueryCategory = () => {
+    //   loading.value = true;
+    //   axios.get("/category/all").then((response) => {
+    //     loading.value = false;
+    //     const data = response.data;
+    //     if (data.success) {
+    //       categorys = data.content;
+    //       console.log("原始数组：", categorys);
+    //
+    //       level1.value = [];
+    //       level1.value = Tool.array2Tree(categorys, 0);
+    //       console.log("树形结构：", level1.value);
+    //
+    //       // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+    //       handleQuery({
+    //         page: 1,
+    //         size: pagination.value.pageSize,
+    //       });
+    //     } else {
+    //       message.error(data.message);
+    //     }
+    //   });
+    // };
 
     const handleQuery = (params: any) => {
       loading.value = true
@@ -220,7 +220,11 @@ export default defineComponent({
       })
     }
     onMounted(() => {
-      handleQueryCategory()
+      // handleQueryCategory()
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize,
+      });
     })
     return {
       loading,
@@ -236,7 +240,7 @@ export default defineComponent({
       modalLoading,
       handleModalOk,
       categoryIds,
-      level1,
+      // level1,
     };
   },
   components: {
