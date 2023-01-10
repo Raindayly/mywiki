1cc2445515bf39954ed6e0ca40526249f190c140
yly <fppuz123456@163.com>
2023-01-10 09:31:50 +0800

    【异常处理】统一异常返回

--- src/main/java/com/yly/wiki/config/ControllerExceptionHandler.java
+++ src/main/java/com/yly/wiki/config/ControllerExceptionHandler.java
@@ -5,9 +5,12 @@ import com.yly.wiki.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * 统一异常处理、数据预处理等
@@ -26,9 +29,18 @@ public class ControllerExceptionHandler {
     @ResponseBody
     public CommonResp validExceptionHandler(BindException e) {
         CommonResp commonResp = new CommonResp();
-        LOG.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+        StringBuilder s = new StringBuilder();
+        int len = 0;
+        while (len < allErrors.size()){
+            s.append(allErrors.get(len).getDefaultMessage());
+            s.append(",");
+            len++;
+        }
+        String errors = s.substring(0,s.length()-1);
+        LOG.warn("参数校验失败：{}", errors);
         commonResp.setSuccess(false);
-        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setMessage(errors);
         return commonResp;
     }
 
