b31e6f6dadd43c9116244090c50b6f9a303e2bc7
yly <fppuz123456@163.com>
2023-01-29 15:41:16 +0800

    集成redis

--- src/main/java/com/yly/wiki/controller/TestController.java
+++ src/main/java/com/yly/wiki/controller/TestController.java
@@ -2,13 +2,18 @@ package com.yly.wiki.controller;
 
 import com.yly.wiki.entity.Test;
 import com.yly.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @version 1.0
@@ -19,6 +24,11 @@ import java.util.List;
 //@Controller//一般用来返回网页
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     /*
     读取自定义配置项 :后面为默认值
@@ -37,5 +47,19 @@ public class TestController {
     @RequestMapping(value = "/list",method = RequestMethod.GET)
     public List<Test> list() {
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
